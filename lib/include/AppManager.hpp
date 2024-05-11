df446a72f3a0512575c01393addb89739a74b844
Guilherme Daros <gh5777.gh@gmail.com>
2024-05-11 15:40:34 -0300

    create AppManager

---
+++ lib/include/AppManager.hpp
@@ -0,0 +1,26 @@
+#include <component/ComponentA.hpp>
+#include <component/ComponentB.hpp>
+
+struct AppConfig {
+  std::string_view name;
+};
+
+class AppManager {
+ public:
+  static auto Instance() -> AppManager& {
+    static AppManager instance;
+    return instance;
+  }
+
+  ~AppManager() = default;
+
+  auto Init(const AppConfig& config) -> void;
+
+  auto MakeComponentA(const infra::component::ComponentAConfig& config) -> void;
+
+  auto MakeComponentB(const infra::component::ComponentBConfig& config) -> void;
+
+ private:
+  std::unique_ptr<infra::component::ComponentA> comp_a_;
+  std::unique_ptr<infra::component::ComponentB> comp_b_;
+};
