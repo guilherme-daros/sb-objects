bf813785d01ec1bc1400d306c70676a1f4c396b3
Guilherme Daros <gh5777.gh@gmail.com>
2024-05-11 15:40:34 -0300

    Add cmake structure to apps

--- apps/App1/Main.cpp
+++ apps/App1/Main.cpp
@@ -1,8 +1,8 @@
 #include <App1Controller.hpp>
-
 #include <memory>
+#include <unistd.h>
 
-int main(int argc, char const* argv[]) {
+int main(int argc, char const *argv[]) {
   auto app = std::make_unique<App1Controller>();
 
   app->Init();
